b8c67600016c615476f8c44d6ae9da5adf1799be
Volodya <chuchman002@gmail.com>
2023-11-30 23:20:33 +0200

    Add project files.

---
+++ Lab №8.1 rec/Lab №8.1 rec.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+bool containsWhile(const std::string& input, const std::string& target, size_t targetIndex) {
+    if (targetIndex == target.length()) {
+        return true; 
+    }
+
+    if (input.find(target[targetIndex]) != std::string::npos) {
+        return containsWhile(input, target, targetIndex + 1);
+    }
+
+    return false; 
+}
+
+bool containsWhile(const std::string& input) {
+    std::string target = "while";
+    return containsWhile(input, target, 0);
+}
+
+void replaceWhile(std::string& input, size_t pos) {
+    if (pos == std::string::npos) {
+        return; 
+    }
+
+    input.replace(pos, 5, "**");
+    replaceWhile(input, input.find("while", pos + 2));
+}
+
+void replaceWhile(std::string& input) {
+    replaceWhile(input, input.find("while"));
+}
+
+int main() {
+    std::string inputString;
+
+    std::cout << "Print line: ";
+    std::getline(std::cin, inputString);
+
+    if (containsWhile(inputString)) {
+        std::cout << "String contains all letters from 'while'.\n";
+    }
+    else {
+        std::cout << "String doesn`t contain all letters with 'while.\n";
+    }
+
+    replaceWhile(inputString);
+
+    std::cout << "Result: " << inputString << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №8.1 rec/Lab №8.1 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestContainsWhile_Negative)
+		{
+			std::string testInput = "randomtext"; 
+
+			// Перевіряємо, чи функція containsWhile повертає false для рядка, який не містить усі літери з "while"
+			Assert::IsFalse(containsWhile(testInput));
+		}
+
+		TEST_METHOD(TestReplaceWhile)
+		{
+			std::string testInput = "while hello while world"; 
+
+			replaceWhile(testInput);
+
+			// Перевіряємо, чи функція replaceWhile виконує заміну правильно
+			Assert::AreEqual(testInput, std::string("** hello ** world"));
+		}
+	};
+}
